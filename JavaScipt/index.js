d35d0d459f29c4c2ae2dce4afe0de351befcf291
Rebecca Mitchelle <rebeccaamitchelle@gmail.com>
2022-10-13 17:41:39 +0300

    Bootstrap

---
+++ JavaScipt/index.js
@@ -0,0 +1,10 @@
+ // Change heading
+ var text;
+ text= "I am a billionaire";
+
+var answer;
+answer="How you doing";
+
+let response = "Good";
+
+ document.getElementById("heading").innerHTML = "Welcome to my world!";
